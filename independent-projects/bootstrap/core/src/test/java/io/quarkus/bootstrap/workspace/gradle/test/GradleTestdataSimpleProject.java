2eef02254059c8f1c99a4ca51699a15f888cf62e
Jesper Skov <jskov@zoftcorp.dk>
2019-05-04 16:44:55 +0200

    Also test output dir

--- independent-projects/bootstrap/core/src/test/java/io/quarkus/bootstrap/workspace/gradle/test/GradleTestdataSimpleProject.java
+++ independent-projects/bootstrap/core/src/test/java/io/quarkus/bootstrap/workspace/gradle/test/GradleTestdataSimpleProject.java
@@ -9,11 +9,11 @@ import java.nio.file.Paths;
 
 import io.quarkus.bootstrap.util.IoUtils;
 
-public class TestdataSimpleProject {
+public class GradleTestdataSimpleProject {
     public static final String DEFAULT_GROUP_ID = "io.quarkus.test";
     public static final String DEFAULT_VERSION = "1.0";
 
-    private TestdataSimpleProject() {}
+    private GradleTestdataSimpleProject() {}
 
     public static void setupSimple(Path workDir) throws IOException {
         IoUtils.copy(Paths.get("test/gradle-workspace/simple"), workDir);
--- independent-projects/bootstrap/core/src/test/java/io/quarkus/bootstrap/workspace/gradle/test/LocalGradleProjectTest.java
+++ independent-projects/bootstrap/core/src/test/java/io/quarkus/bootstrap/workspace/gradle/test/LocalGradleProjectTest.java
@@ -3,17 +3,21 @@
  */
 package io.quarkus.bootstrap.workspace.gradle.test;
 
-import static io.quarkus.bootstrap.workspace.gradle.test.TestdataSimpleProject.cleanupSimple;
-import static io.quarkus.bootstrap.workspace.gradle.test.TestdataSimpleProject.setupSimple;
+import static io.quarkus.bootstrap.workspace.gradle.test.GradleTestdataSimpleProject.DEFAULT_GROUP_ID;
+import static io.quarkus.bootstrap.workspace.gradle.test.GradleTestdataSimpleProject.DEFAULT_VERSION;
+import static io.quarkus.bootstrap.workspace.gradle.test.GradleTestdataSimpleProject.cleanupSimple;
+import static io.quarkus.bootstrap.workspace.gradle.test.GradleTestdataSimpleProject.setupSimple;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 import java.nio.file.Path;
+import java.util.Map;
 
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import io.quarkus.bootstrap.model.AppArtifactKey;
 import io.quarkus.bootstrap.resolver.LocalProject;
 import io.quarkus.bootstrap.resolver.gradle.workspace.LocalGradleProject;
 import io.quarkus.bootstrap.util.IoUtils;
@@ -33,12 +37,24 @@ public class LocalGradleProjectTest {
     }
 
     @Test
-    public void extractClasspathFromSimpleProject() throws Exception {
+    public void canExtractOutputDirFromSimpleProject() throws Exception {
         Path buildDir = workDir.resolve("build");
         final LocalProject project = LocalGradleProject.load(buildDir.resolve("classes"));
         assertNotNull(project);
         
         assertEquals(buildDir, project.getOutputDir());
-
+    }
+    
+    @Test
+    public void canLoadSimpleProject() throws Exception {
+        final LocalProject project = LocalGradleProject.load(workDir.resolve("build").resolve("classes"));
+        assertNotNull(project);
+        assertNotNull(project.getWorkspace());
+        assertEquals(DEFAULT_GROUP_ID, project.getGroupId());
+        assertEquals("simple", project.getArtifactId());
+        assertEquals(DEFAULT_VERSION, project.getVersion());
+        final Map<AppArtifactKey, LocalProject> projects = project.getWorkspace().getProjects();
+        assertEquals(1, projects.size());
+        projects.containsKey(new AppArtifactKey(DEFAULT_GROUP_ID, "simple"));
     }
 }
