913a41fa04f3085fa4c4391909ad84cfcb8c90c5
Alexey Loubyansky <olubyans@redhat.com>
2021-09-09 15:57:01 +0200

    minor

--- independent-projects/tools/devtools-common/src/main/java/io/quarkus/devtools/project/info/ProjectInfo.java
+++ independent-projects/tools/devtools-common/src/main/java/io/quarkus/devtools/project/info/ProjectInfo.java
@@ -1,29 +1,29 @@
 package io.quarkus.devtools.project.info;
 
-import java.util.Collection;
 import io.quarkus.maven.ArtifactCoords;
+import java.util.Collection;
 
 public interface ProjectInfo {
 
-	/**
-	 * Collection of imported platform BOMs in the order they are imported.
-	 *
-	 * @return  imported platform BOMs
-	 */
-	Collection<ArtifactCoords> getImportedPlatformBoms();
+    /**
+     * Collection of imported platform BOMs in the order they are imported.
+     *
+     * @return imported platform BOMs
+     */
+    Collection<ArtifactCoords> getImportedPlatformBoms();
 
-	/**
-	 * Collection of direct extension dependencies that are managed by the imported platform BOMs.
-	 *
-	 * @return  collection of direct platform extension dependencies
-	 */
-	Collection<ArtifactCoords> getPlatformExtensions();
+    /**
+     * Collection of direct extension dependencies that are managed by the imported platform BOMs.
+     *
+     * @return collection of direct platform extension dependencies
+     */
+    Collection<ArtifactCoords> getPlatformExtensions();
 
-	/**
-	 * Collection of direct extension dependencies that are not managed by the imported platform BOMs.
-	 *
-	 * @return  collection of direct non-platform extension dependencies
-	 */
-	Collection<ArtifactCoords> getNonPlatformExtensions();
+    /**
+     * Collection of direct extension dependencies that are not managed by the imported platform BOMs.
+     *
+     * @return collection of direct non-platform extension dependencies
+     */
+    Collection<ArtifactCoords> getNonPlatformExtensions();
 
 }
