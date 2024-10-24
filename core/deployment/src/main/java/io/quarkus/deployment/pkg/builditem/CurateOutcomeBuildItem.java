f9aade715dd19f3f97e96c87b88fff239a46d71b
Alexey Loubyansky <olubyans@redhat.com>
2024-10-24 23:00:11 +0200

    wip

--- core/deployment/src/main/java/io/quarkus/deployment/pkg/builditem/CurateOutcomeBuildItem.java
+++ core/deployment/src/main/java/io/quarkus/deployment/pkg/builditem/CurateOutcomeBuildItem.java
@@ -1,6 +1,9 @@
 package io.quarkus.deployment.pkg.builditem;
 
+import java.nio.file.Path;
+
 import io.quarkus.bootstrap.model.ApplicationModel;
+import io.quarkus.bootstrap.workspace.WorkspaceModule;
 import io.quarkus.builder.item.SimpleBuildItem;
 
 public final class CurateOutcomeBuildItem extends SimpleBuildItem {
@@ -12,6 +15,31 @@ public final class CurateOutcomeBuildItem extends SimpleBuildItem {
     }
 
     public ApplicationModel getApplicationModel() {
+        //        System.out.println("APP MODULE " + getApplicationModuleDirectory() + ", module available "
+        //               + (appModel.getApplicationModule() != null));
         return appModel;
+    }
+
+    /**
+     * Returns the path to the application module directory.
+     * For a single module project it will the project directory. For a multimodule project,
+     * it will the directory of the application module.
+     *
+     * @return application module directory, never null
+     */
+    public Path getApplicationModuleDirectory() {
+        final WorkspaceModule module = appModel.getApplicationModule();
+        // modules are by default available in dev and test modes, and in prod mode if
+        // quarkus.bootstrap.workspace-discovery system or project property is true,
+        // otherwise it could be null
+        if (module != null) {
+            return module.getModuleDir().toPath();
+        }
+        final String basedir = System.getProperty("basedir");
+        if (basedir != null) {
+            return Path.of(basedir);
+        }
+        // if the module isn't available, return the current directory
+        return Path.of("").toAbsolutePath();
     }
 }
